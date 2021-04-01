fabac29c1e5a54e2e58f9f2e56e6ddab0a1cf0ad
DongWooE <ehddn977@naver.com>
2021-04-01 15:41:38 +0900

    #4 manual activity 추가

---
+++ Android/app/src/main/java/com/mobeom/mobeom/ManualActivity.java
@@ -0,0 +1,14 @@
+package com.mobeom.mobeom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManualActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual);
+    }
+}
