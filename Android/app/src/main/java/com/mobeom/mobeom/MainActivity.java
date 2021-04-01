1121751de31323943fe36c201e683990096c6414
DongWooE <ehddn977@naver.com>
2021-04-01 15:23:55 +0900

    #4 메인화면 초기 설정
    메인 화면에 버튼 5개 추가
    exit를 누를 시에 어플 종료 코드 추가
    manual로 넘어갈 수 있도록 intent 추가

--- Android/app/src/main/java/com/mobeom/mobeom/MainActivity.java
+++ Android/app/src/main/java/com/mobeom/mobeom/MainActivity.java
@@ -2,13 +2,45 @@ package com.mobeom.mobeom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv_title;
+    Button bt_manual;
+    Button bt_maskDetector;
+    Button bt_autoDiagnosis;
+    Button bt_exit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_title = findViewById(R.id.tv_title);
+        bt_manual = findViewById(R.id.bt_manual);
+        bt_maskDetector = findViewById(R.id.bt_maskDetector);
+        bt_autoDiagnosis = findViewById(R.id.bt_autoDiagnosis);
+        bt_exit = findViewById(R.id.bt_exit);
+
+        bt_manual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ManualActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        bt_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+            }
+        });
     }
 }
