3ef2bb89ac8956392e07cf20f67c62ffcc3e20bb
DongWooE <ehddn977@naver.com>
2021-05-11 01:06:14 +0900

    #23 Add google map

--- app/src/main/java/org/tensorflow/lite/examples/classification/Presentation/HealthCenterActivity/HealthCenterActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/Presentation/HealthCenterActivity/HealthCenterActivity.java
@@ -1,25 +1,50 @@
 package org.tensorflow.lite.examples.classification.Presentation.HealthCenterActivity;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.tensorflow.lite.examples.classification.R;
 import org.tensorflow.lite.examples.classification.databinding.ActivityCheckListBinding;
 
 
-public class HealthCenterActivity extends AppCompatActivity {
+public class HealthCenterActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private ActivityCheckListBinding binding;
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_health_center);
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_health_center);
 
-        
+        fragmentManager = getFragmentManager();
+        mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.googleMap);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng location = new LatLng(37.50114643284602, 126.95343165838096);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("우리집");
+        markerOptions.snippet("우리집 앞");     //세부 설명
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16)); // 가까이 보고 싶으면 숫자를 올린다
     }
 }
+
