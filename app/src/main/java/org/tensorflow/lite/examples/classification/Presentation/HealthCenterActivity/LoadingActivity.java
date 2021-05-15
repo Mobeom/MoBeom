67c9b8fe6b914e51b4e28cbb6dd6ef041139849c
DongWooE <ehddn977@naver.com>
2021-05-15 19:18:59 +0900

    #23 Modify HealthCenter and Loading Activities

--- app/src/main/java/org/tensorflow/lite/examples/classification/Presentation/HealthCenterActivity/LoadingActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/Presentation/HealthCenterActivity/LoadingActivity.java
@@ -1,14 +1,71 @@
 package org.tensorflow.lite.examples.classification.Presentation.HealthCenterActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.tensorflow.lite.examples.classification.Presentation.HealthCenterActivity.Data.ApiService;
+import org.tensorflow.lite.examples.classification.Presentation.HealthCenterActivity.Data.ConstantValue;
+import org.tensorflow.lite.examples.classification.Presentation.HealthCenterActivity.Data.SelectiveClinicJson;
+import org.tensorflow.lite.examples.classification.R;
+
+import java.io.Serializable;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoadingActivity extends AppCompatActivity {
+
+    public HealthCenterController controller;
+    public List<SelectiveClinicJson> arrayList;
+    public List<SelectiveClinicJson> resource;
+    static final String BASE_URL = ConstantValue.Url.BASE_URL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        ApiService apiService = retrofit.create(ApiService.class);
+
+        Call<List<SelectiveClinicJson>> call = apiService.getSelectiveClinics();
+        call.enqueue(new Callback<List<SelectiveClinicJson>>() {
+            @Override
+            public void onResponse(Call<List<SelectiveClinicJson>> call, Response<List<SelectiveClinicJson>> response) {
+                if (response.isSuccessful()) {
+                    Log.e("TAG", "Server response code :: " + response.code() + " ");
+                    resource = response.body();
+                    Intent intent = new Intent(LoadingActivity.this, HealthCenterActivity.class);
+                    intent.putExtra("clinics", (Serializable)resource);
+                    startActivity(intent);
+                } else {
+                    System.out.println(response.errorBody());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<SelectiveClinicJson>> call, Throwable t) {
+                call.cancel();
+                t.printStackTrace();
+            }
+        });
     }
 }
--- app/src/main/java/org/tensorflow/lite/examples/classification/Presentation/MainActivity/MainActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/Presentation/MainActivity/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,11 +15,10 @@ import androidx.databinding.DataBindingUtil;
 import org.tensorflow.lite.examples.classification.Data.API.CoronaAPI;
 import org.tensorflow.lite.examples.classification.Data.API.CoronaStatus;
 import org.tensorflow.lite.examples.classification.Presentation.CheckListActivity.CheckListActivity;
-import org.tensorflow.lite.examples.classification.Presentation.HealthCenterActivity.HealthCenterActivity;
+import org.tensorflow.lite.examples.classification.Presentation.HealthCenterActivity.LoadingActivity;
 import org.tensorflow.lite.examples.classification.Presentation.MaskDetectionActivity.ClassifierActivity;
 import org.tensorflow.lite.examples.classification.R;
 import org.tensorflow.lite.examples.classification.databinding.ActivityMainBinding;
-import org.xml.sax.SAXException;
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
@@ -28,8 +26,6 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
-
-import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -53,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         binding.buttonHealthCenter.setOnClickListener(view -> {
-            Intent intent = new Intent(getApplicationContext(), HealthCenterActivity.class);
+            Intent intent = new Intent(getApplicationContext(), LoadingActivity.class);
             if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 0); }
             else startActivity(intent);
