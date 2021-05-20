43722396dbad5acff90e16aa1707625d53629e77
Lee WonJoong <realjoong@gmail.com>
2021-05-20 17:58:28 +0900

    [ADD] (#19) - Splash, Main Activity 색 수정

--- app/src/main/java/org/tensorflow/lite/examples/classification/Presentation/SplashActivity/ThirdFragment.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/Presentation/SplashActivity/ThirdFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
