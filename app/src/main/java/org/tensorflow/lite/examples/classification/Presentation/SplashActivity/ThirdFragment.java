e517dd7b619a58350d3eb2ba1a01d25d6583b63b
Lee WonJoong <realjoong@gmail.com>
2021-05-20 22:02:01 +0900

    [ADD] (#19) - Animation 추가
    - Splash Activity Third Fragment에 Animation 추가.

--- app/src/main/java/org/tensorflow/lite/examples/classification/Presentation/SplashActivity/ThirdFragment.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/Presentation/SplashActivity/ThirdFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.motion.widget.MotionLayout;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
@@ -23,15 +24,28 @@ public class ThirdFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_third, container, false);
-        onEnterButtonClick();
-        //return inflater.inflate(R.layout.fragment_third, container, false);
-        return binding.getRoot();
-    }
+        binding.ml.setTransitionListener(new MotionLayout.TransitionListener() {
+            @Override
+            public void onTransitionStarted(MotionLayout motionLayout, int startId, int endId) {
 
-    private void onEnterButtonClick() {
-        binding.btEnterApp.setOnClickListener(view -> {
-            Intent intent = new Intent(getContext(), MainActivity.class);
-            startActivity(intent);
+            }
+
+            @Override
+            public void onTransitionChange(MotionLayout motionLayout, int startId, int endId, float progress) {
+
+            }
+
+            @Override
+            public void onTransitionCompleted(MotionLayout motionLayout, int currentId) {
+                Intent intent = new Intent(getContext(), MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onTransitionTrigger(MotionLayout motionLayout, int triggerId, boolean positive, float progress) {
+
+            }
         });
+        return binding.getRoot();
     }
 }
