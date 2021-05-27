fa0b363756980166ae9a5ae73dbcb82133738998
DongWooE <ehddn977@naver.com>
2021-05-27 23:32:16 +0900

    [ADD] #41 - add server schema files

---
+++ app/src/main/java/org/tensorflow/lite/examples/classification/Presentation/CheckListActivity/Data/UserInfoJson.java
@@ -0,0 +1,4 @@
+package org.tensorflow.lite.examples.classification.Presentation.CheckListActivity.Data;
+
+public class UserInfoJson {
+}
---
+++ app/src/main/java/org/tensorflow/lite/examples/classification/Presentation/HealthCenterActivity/Data/SecretValues.java
@@ -0,0 +1,4 @@
+package org.tensorflow.lite.examples.classification.Presentation.HealthCenterActivity.Data;
+
+public class SecretValues {
+}
