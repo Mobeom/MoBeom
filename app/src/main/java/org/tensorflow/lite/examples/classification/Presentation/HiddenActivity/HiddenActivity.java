4b8a64561ec499dc4a4cc8622d7335127f7a84fe
Lee WonJoong <realjoong@gmail.com>
2021-05-28 22:20:19 +0900

    [ADD] (#37) - Item click 시 깃헙 페이지 이동 기능 추가

--- app/src/main/java/org/tensorflow/lite/examples/classification/Presentation/HiddenActivity/HiddenActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/Presentation/HiddenActivity/HiddenActivity.java
@@ -1,5 +1,7 @@
 package org.tensorflow.lite.examples.classification.Presentation.HiddenActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -16,5 +18,22 @@ public class HiddenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_hidden);
         setContentView(binding.getRoot());
+
+        binding.clDongwoo.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/DongWooE"));
+            startActivity(intent);
+        });
+        binding.clSeongjin.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/L-o-g-a-n"));
+            startActivity(intent);
+        });
+        binding.clGyubbin.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/gyub99"));
+            startActivity(intent);
+        });
+        binding.clWonjoong.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/WonJoongLee"));
+            startActivity(intent);
+        });
     }
 }
